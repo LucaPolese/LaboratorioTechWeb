13efde7696a70a9b11ed44125ae763a907e23370
Luca Polese <luca.polese@outlook.com>
2021-12-21 08:49:15 +0100

    Aggiunti Files della lezione 15/12

---
+++ pages/nuovoProtagonistaControlli.php
@@ -0,0 +1,68 @@
+<?php
+    require_once "db.php";
+    use DB\DBAccess;
+
+    $paginaHTML file_get_contents("nuovoProtagonistaForm.php");
+
+    $messaggi= "";
+    $nome ="";
+    $colore="";
+    $peso="";
+    $potenza="";
+    $descrizione="";
+    $edizioni = array();
+
+    if(isset($_POST['submit'])){
+        //l'utente ha premuto almeno una volta il pulsante submit
+        $nome = $_POST['nome'];
+        if(!strlen($nome)){
+            $messaggi .= "<p>Il nome non può essere vuoto</p>";
+        }elseif(preg_match('/\d/', $nome)){
+            $messaggi .= "<p>Il nome non può contenere numeri</p>";
+        }))   
+
+        $colore = $_POST['colore'];
+        if(!strlen($colore)){
+            $messaggi .= "<p>Il colore non può essere vuoto</p>";
+        }elseif(preg_match('/\d/', $nome)){
+            $messaggi .= "<p>Il colore non può contenere numeri</p>";
+        }))   
+
+        $peso = $_POST['peso'];
+        if(!strlen($peso)){
+            $messaggi .= "<p>Il peso non può essere vuoto</p>";
+        }else if(!ctype_digit($peso)){
+            $messaggi .= "<p>Il peso deve essere un numero</p>";
+        }
+
+        $potenza = $_POST['potenza'];
+        if(!strlen($potenza)){
+            $messaggi .= "<p>Il potenza non può essere vuoto</p>";
+        }
+
+        $descrizione = $_POST['descrizione'];
+        $edizioni = $_POST['edizioni'];
+
+    }
+    //prima lettura dei dati, oppure ho già premuto il pulsante submit, ma mantengo i dati nella form con messaggio di avvenuto successo
+    $paginaHTML = str_replace("<messaggioForm/>", $messaggi, $paginaHTML);
+    $paginaHTML = str_replace("<valoreNome/>", $nome, $paginaHTML);
+    $paginaHTML = str_replace("<valoreColore/>", $colore, $paginaHTML);
+    $paginaHTML = str_replace("<valorePeso/>", $peso, $paginaHTML);
+    $paginaHTML = str_replace("<vaoreDescrizione/>", $descrizione, $paginaHTML);
+    $paginaHTML = str_replace("<valorePotenza/>", $potenza, $paginaHTML);
+
+
+    /* RICONTROLLARE */
+
+    $connessione = new DBAccess();
+    $connessioneOK = $connessione->openDBConnection();
+
+    if($connessioneOK){
+        $risultatoQuery = $connessione->insertNewCharacter($nome, $colore, $peso, $potenza, $descrizione, $ab, $abr, $absw, $abs);
+        if($risultatoQuery){
+            $messaggi .= "<p> Il personaggio è stato inserito correttamente </p>";
+        }else{
+            $messaggi .= "<p> Errore nell\' inserimento</p>";
+        }
+?>
--- pages/protagonisti.php
+++ pages/protagonisti.php
@@ -7,8 +7,26 @@
     $connessione = new DBAccess();
     $connessioneOK = $connessione->openDBConnection();
 
-    $personaggi = "";
+    $personaggi = ""; /*dati grezzi dal DB*/
+    $listaPersonaggi = ""; /* codice HTML da dare in output */
 
-    $listaProtagonisti = getCharacterList();
+    if($connessioneOK){
+        $personaggi = $connessione->getCharacterList();
+        $connessione->closeDBConnection();
+        //Devo ancora controllare se $personaggi è nullo
+        if($personaggi != null){
+            $listaProtagonisti = '<dl id="charactersStory">';
+            foreach($personaggi as $personaggio){
+                $listaPersonaggi .= '<dt xml:lang="en">'.$personaggio['nome'].'</dt>';
+                $listaPersonaggi .= '<dd>''</dd>';
+                $listaPersonaggi .= '<dd><img src="../images/'.$personaggio['immagine'].'"/><p>'.$personaggio['descrizione'].'</p></dd>';
+            }
+            $listaPersonaggi .= "</dl>";
+        }else{
+            $listaPersonaggi = "<p> Non ci sono informazioni relative ai personaggi </p>";
+        }
+    }else{
+        $listaPersonaggi = "<p> I sistemi sono al momento non disponibili, riprova più tardi </p>";
+    }
     echo str_replace("<listaProtagonisti/>", $listaProtagonisti, $paginaHTML);
 ?>
